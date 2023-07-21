d69324e30a423ea1007c38e88b9be26857567022
Olexander Hofman <oh@mamoru.network>
2023-07-21 16:39:06 +0200

    -enable topCall true

--- mempool/sniffer_txpool.go
+++ mempool/sniffer_txpool.go
@@ -155,7 +155,7 @@ func (bc *SnifferBackend) process(ctx context.Context, header *types.Header, txs
 	stateDb = stateDb.Copy()
 
 	for index, tx := range txs {
-		calltracer, err := mamoru.NewCallTracer(true)
+		calltracer, err := mamoru.NewCallTracer(false)
 		if err != nil {
 			log.Error("Mamoru Call tracer", "err", err, "ctx", mamoru.CtxTxpool)
 		}
